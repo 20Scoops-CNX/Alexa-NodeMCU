906f2466239cb09a4f2649127a9dc0542fb714ca
Jedsada Tiwongvorakul <jedsada@wisdomlanna.com>
2017-02-20 00:36:21 +0700

    first commit

---
+++ wemos/wemos.ino
@@ -0,0 +1,115 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <WiFiUdp.h>
+#include <functional>
+#include "switch.h"
+#include "UpnpBroadcastResponder.h"
+#include "CallbackFunction.h"
+
+// prototypes
+boolean connectWifi();
+
+//on/off callbacks 
+void officeLightsOn();
+void officeLightsOff();
+void kitchenLightsOn();
+void kitchenLightsOff();
+
+// Change this before you flash
+const char* ssid = "pondthaitay";
+const char* password = "55021744";
+
+boolean wifiConnected = false;
+
+int relayOne = 13;
+
+UpnpBroadcastResponder upnpBroadcastResponder;
+
+Switch *office = NULL;
+Switch *kitchen = NULL;
+
+void setup()
+{
+  Serial.begin(9600);
+   
+  // Initialise wifi connection
+  wifiConnected = connectWifi();
+  
+  if(wifiConnected){
+    upnpBroadcastResponder.beginUdpMulticast();
+    
+    // Define your switches here. Max 14
+    // Format: Alexa invocation name, local port no, on callback, off callback
+    office = new Switch("Light Campus", 80, officeLightsOn, officeLightsOff);
+    kitchen = new Switch("Light Two", 81, kitchenLightsOn, kitchenLightsOff);
+
+    Serial.println("Adding switches upnp broadcast responder");
+    upnpBroadcastResponder.addDevice(*office);
+    upnpBroadcastResponder.addDevice(*kitchen);
+
+    pinMode(13, OUTPUT);
+    digitalWrite(relayOne, HIGH);
+  }
+}
+ 
+void loop()
+{
+	 if(wifiConnected){
+      upnpBroadcastResponder.serverLoop();
+      kitchen->serverLoop();
+      office->serverLoop();
+	 }
+}
+
+void officeLightsOn() {
+    digitalWrite(relayOne, LOW);
+}
+
+void officeLightsOff() {
+   digitalWrite(relayOne, HIGH);
+}
+
+void kitchenLightsOn() {
+  digitalWrite(relayOne, LOW);
+}
+
+void kitchenLightsOff() {
+  digitalWrite(relayOne, HIGH);
+}
+
+// connect to wifi – returns true if successful or false if not
+boolean connectWifi(){
+  boolean state = true;
+  int i = 0;
+  
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+  Serial.println("Connecting to WiFi");
+
+  // Wait for connection
+  Serial.print("Connecting ...");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    if (i > 10){
+      state = false;
+      break;
+    }
+    i++;
+  }
+  
+  if (state){
+    Serial.println("");
+    Serial.print("Connected to ");
+    Serial.println(ssid);
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+  }
+  else {
+    Serial.println("");
+    Serial.println("Connection failed.");
+  }
+  
+  return state;
+}
